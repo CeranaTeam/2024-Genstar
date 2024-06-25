d267b44cffac6f3a6ea73841465102f7d1a44f9c
YJack0000 <jackleagends@gmail.com>
2024-06-25 22:52:26 +0800

    feat: add @ alias

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import url from 'url'
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            // These are the aliases we set up in tsconfig.json
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 })
