139869eed92a2f1de92f2ca3cc126899ef4c012c
yui0303 <iampeter62@gmail.com>
2024-07-01 00:33:53 +0800

    feat: add type for drugs

---
+++ ui/src/types/drug.d.ts
@@ -0,0 +1,10 @@
+type Drug = {
+  id: string;
+  name: string;
+  mainIngredient: string;
+  mainIngredientQuantity: string;
+  manufacturer: string;
+  price: string;
+  dosage: string;
+  compound: "Y" | "N";
+};
