faed90b88c1a244237c370c4d915febf46bb12bf
YJack0000 <jackleagends@gmail.com>
2024-07-04 23:20:20 +0800

    feat: implement part of summary md

--- ui/src/pages/home.tsx
+++ ui/src/pages/home.tsx
@@ -1,11 +1,13 @@
 import DiagnoseTextAreas from "@/components/system/DiagnoseTextAreas"
 import LLMResponseTables from "@/components/system/LLMResponseTables"
+import SummarySection from "@/components/system/SummarySection";
 
 function Home() {
   return (
     <>
       <DiagnoseTextAreas />
       <LLMResponseTables />
+      <SummarySection />
     </>
   );
 }
