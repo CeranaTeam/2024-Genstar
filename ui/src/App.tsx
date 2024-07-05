f7c0d5b5f3f003b44b4bf2325613c18fb7354aaf
YJack0000 <jackleagends@gmail.com>
2024-07-05 23:28:43 +0800

    feat: temparary remove login

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/home"
-import Login from "@/pages/login"
-import ProtectedRoute from "@/lib/protected-routes"
+//import Login from "@/pages/login"
+//import ProtectedRoute from "@/lib/protected-routes"
 import Layout from "@/components/Layout"
 import LLMResponsesProvider from "./components/store/LLMResponsesProvider";
 
@@ -9,16 +9,13 @@ function App() {
 
   return (
     <LLMResponsesProvider>
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<ProtectedRoute />} >
-              <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
-      </Layout>
-    </Router>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+      </Router>
     </LLMResponsesProvider>
   )
 }
