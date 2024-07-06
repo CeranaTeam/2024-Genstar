de5cd3daf47ac009c632c86f578dcac012bd019b
YJack0000 <jackleagends@gmail.com>
2024-07-06 20:22:22 +0800

    feat: update Title

--- ui/src/components/Layout.tsx
+++ ui/src/components/Layout.tsx
@@ -4,7 +4,7 @@ const Layout: React.FC<LayoutRouteProps> = ({ children }) => {
   return (
     <>
       <div>
-        <h1 className='text-3xl font-bold text-gray-800 py-2 px-4 border-b'>MediGuess Assistant (beta)</h1>
+        <h1 className='text-3xl font-bold text-gray-800 py-2 px-4 border-b'>MediCopilot: 病例撰寫副駕駛</h1>
       </div>
       <div className='px-4 max-w-[1000px]'>
         {children}
